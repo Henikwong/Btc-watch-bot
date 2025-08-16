27afb7a483abbe8ef566d448d2bcdf05af51fae6
Henikwong <161950153+Henikwong@users.noreply.github.com>
2025-08-17 02:25:25 +0800

    Delete get_chatid.py

--- get_chatid.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-TELEGRAM_BOT_TOKEN =8201207952:AAGxeasWHR6u-l3SiNyf5h4U7Pa4oQTsEAM
-
-url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/getUpdates"
-r = requests.get(url)
-print(r.text)
