227a5410568fc901a335cde4b032b6c7127f68f3
Henikwong <161950153+Henikwong@users.noreply.github.com>
2025-08-17 00:01:30 +0800

    get_chatid.py

--- get_chatid.py
+++ get_chatid.py
@@ -1,6 +1,6 @@
 import requests
 
-TELEGRAM_BOT_TOKEN = "在这里填你BotFather给的token"
+TELEGRAM_BOT_TOKEN =8201207952:AAGxeasWHR6u-l3SiNyf5h4U7Pa4oQTsEAM
 
 url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/getUpdates"
 r = requests.get(url)
