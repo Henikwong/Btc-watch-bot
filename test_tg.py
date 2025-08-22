3b16374e22d74b50e79fd88c4b22f995b145febd
Henikwong <161950153+Henikwong@users.noreply.github.com>
2025-08-22 23:44:44 +0800

    Update test_tg.py

--- test_tg.py
+++ test_tg.py
@@ -1,10 +1,8 @@
 import os, requests
 
-token = os.getenv("TELEGRAM_BOT_TOKEN", "你的token")
-chat_id = os.getenv("TELEGRAM_CHAT_ID", "你的chatid")
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-resp = requests.get(
-    f"https://api.telegram.org/bot{token}/sendMessage",
-    params={"chat_id": chat_id, "text": "测试消息 from Railway"}
-)
+resp = requests.get(f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+                    params={"chat_id": CHAT_ID, "text": "测试消息 from Railway"})
 print(resp.json())
