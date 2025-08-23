5cc60bce004285f0ea3ff329917fd6b31df88696
Henikwong <161950153+Henikwong@users.noreply.github.com>
2025-08-23 16:49:32 +0800

    Delete test_tg.py

--- test_tg.py
+++
@@ -1,8 +0,0 @@
-import os, requests
-
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
-resp = requests.get(f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-                    params={"chat_id": CHAT_ID, "text": "测试消息 from Railway"})
-print(resp.json())
